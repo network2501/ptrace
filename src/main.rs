8c882da0868dcabde1519bde8256a8a800850814
network2501 <pete@network2501.com>
2022-05-04 22:40:14 +0800

    need to keep reading through rust in action, want to understand bitflags and how i can apply them

--- src/main.rs
+++ src/main.rs
@@ -32,36 +32,45 @@
 
 use std::net::Ipv4Addr;
 
+// wanna xhibit this and nest the bitflags struct in the Ipv4Header struct but SO says no
+
+struct Ipv4Flags {
+    reserved: 0,
+    dont_fragment: u8,
+    more_fragments: u8
+}
 
 // setting u8 for 4bits or 3bits seems wasteful would like to shrink that if possible.
 struct Ipv4Header {
-    version u8,
+    version: u8,
     internet_header_length: u8,
     differentiated_services_code_point: u8,
     explicit_congestion_notification: u8,
-    total_length: u16,
+    // total_length: u16,
     identification: u16,
-    flags: u8,
-    fragment_offset: u16,
+    flags: BitlFags,
+    // fragment_offset: u16,
     time_to_live: u8,
-    protocl: u8,
+    protocol: u8,
     header_checksum: u16,
-    source_address: u32,
-    destination_addres: u32,
+    source_address: Ipv4Addr,
+    destination_addres: Ipv4Addr,
 }
 
 impl Ipv4Header {
     fn new(src: Ipv4Addr, dst: Ipv4Addr) -> Self {
         Ipv4Header {
             version: 4,
-            explicit_congestion_notification: 0,
+            internet_header_length: 0,
             differentiated_services_code_point: 0,
+            explicit_congestion_notification: 0,
             identification: 0,
-            // <reading about bitmask flags intensifies>
-            flags:
-
-
-
+            flags: ,
+            time_to_live: 0,
+            protocol: 4,
+            header_checksum: 'something',
+            source_address: src,
+            destination_addres: dst
         }
     }
 }
